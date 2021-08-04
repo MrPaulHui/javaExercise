2f6b4615606a83fca7aec7d97e2da598e5c7b613
MrPaulHui <paulhuiabc@163.com>
2021-08-04 12:16:40 +0800

    add exception all

--- src/javaExercise/ExceptionDeal.java
+++ src/javaExercise/ExceptionDeal.java
@@ -7,8 +7,26 @@ public class ExceptionDeal {
 		// TODO Auto-generated method stub
 		System.out.println(PracticeException.pratice1());
 		Account a1 = new Account(100);
+		a1.deposit(2);
 		try {
 			a1.withdraw(105);
+		}catch (OverdraftException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace(); //定位异常位置，debug很有用的
+			System.out.println("透支金额为"+e.getDeficit());
+		}
+		Account a2 = new CheckAccount(100, 10, 10);
+		a2.deposit(5);
+		try {
+			a2.withdraw(111); //这里用到了多态的特性，即在运行中实际调用的是CheckAccount类的withdraw
+		}catch (OverdraftException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace(); //定位异常位置，debug很有用的
+			System.out.println("透支金额为"+e.getDeficit());
+		}
+		a2.deposit(4);
+		try {
+			a2.withdraw(5); //这里用到了多态的特性，即在运行中实际调用的是CheckAccount类的withdraw
 		}catch (OverdraftException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace(); //定位异常位置，debug很有用的
@@ -33,7 +51,7 @@ class PracticeException{
 }
 
 class Account{
-	private double balance;
+	protected double balance;
 	public Account(double bal) {
 		this.balance = bal;
 	}
@@ -42,17 +60,58 @@ class Account{
 	}
 	public void deposit(double money) {
 		this.balance += money;
+		System.out.println("存款"+money+"，余额为"+this.balance);
 	}
 	public void withdraw(double money) throws OverdraftException { //后面的throws一定要加
 		if(this.balance-money<0) {
 			double deficit = money-this.balance;
-			throw new OverdraftException("已透支，无法取款", deficit);
+			throw new OverdraftException("无法取款", deficit);
 		}
 		else {
 			this.balance -= money;
+			System.out.println("取款"+money+"，余额为"+this.balance);
 		}
 	}
 }
+
+class CheckAccount extends Account{ //信用卡，哈哈哈
+	private double overdraftProtect;
+	private double maxProtect;
+	public CheckAccount(double bal) {
+		super(bal);
+	}
+	public CheckAccount(double bal, double protect, double maxp) {
+		super(bal);
+		this.overdraftProtect = protect;
+		this.maxProtect = maxp;
+	}
+	@Override
+	public void deposit(double money) {
+		if(this.overdraftProtect<this.maxProtect) {
+			this.overdraftProtect += money;
+			if(this.overdraftProtect>this.maxProtect) {
+				this.overdraftProtect = this.maxProtect;
+			}
+		}
+		this.balance += money;
+		System.out.println("存款"+money+"，余额为"+this.balance+"，信用额度为"+this.overdraftProtect);
+	}
+	@Override
+	public void withdraw(double money) throws OverdraftException{
+		if(this.balance+this.overdraftProtect-money<0) {
+			double deficit = money-this.balance;
+			throw new OverdraftException("超过透支额度，无法取款", deficit);
+		}
+		else {
+			if(this.balance<money) {
+				this.overdraftProtect -= money-this.balance;  
+			}
+			this.balance -= money;
+			System.out.println("取款"+money+"，余额为"+this.balance+"，信用额度为"+this.overdraftProtect);
+		}
+	}
+}
+
 /*
 class OverdraftException extends Exception{ //自定义异常，只能在一个新java文件里定义为public class，不然会报错
 	private double deficit;
